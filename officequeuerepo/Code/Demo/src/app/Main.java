1ed61a6215c5e7c81a589a9a18207e7423b790f6
lucen <s236834@studenti.polito.it>
2020-10-21 15:10:01 +0200

    Fixing Date

--- officequeuerepo/Code/Demo/src/app/Main.java
+++ officequeuerepo/Code/Demo/src/app/Main.java
@@ -4,9 +4,8 @@ import java.sql.SQLException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import java.text.ParseException;
+
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Calendar;
 
 
@@ -16,8 +15,7 @@ public class Main {
 		try {
 			//Vars
 			Service service = new Service();
-			boolean running = true;
-			boolean running_date=true;
+			boolean running=true;
 			Scanner scanner = new Scanner(System.in);
 
 			//Configuration Phase
@@ -38,7 +36,7 @@ public class Main {
 			}
 			
 			while (true) {
-				System.out.print("Do you want to add a counter?\n ");
+				System.out.print("Do you want to add a counter?\n");
 				System.out.println("1 - Yes\n");
 				System.out.println("2 - No\n");
 				if (scanner.nextInt() == 1) {
@@ -54,72 +52,58 @@ public class Main {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			//Use calendar
 			Calendar cal = Calendar.getInstance();
-			while(running_date){
-				System.out.println("Do you work today?\n");
-				System.out.println("1 - Yes\n");
-				System.out.println("2 - No\n");
+			String date = sdf.format(cal.getTime()); //date in string
+			service.resetQueues();
+			while(running) {
+				System.out.println("Day: "+date+"\n");
+				System.out.println("--- Welcome to the office! Choose an option:\n" +
+						"1 - Get a ticket\n" +
+						"2 - Employee actions\n" +
+						"3 - Manager actions\n" +
+						"4 - Exit ( JUST FOR DEMO )");
 				input = scanner.nextInt(); //Get input from console
-				switch(input){
+				//Menu handler
+				switch (input){
 					case 1:
-						//add 1 day to the date
-						cal.add(Calendar.DAY_OF_MONTH, 1);
-						String date = sdf.format(cal.getTime()); //date in string
-						service.resetQueues();
-						while(running) {
-							System.out.println("Day: "+date+"\n");
-							System.out.println("--- Welcome to the office! Choose an option:\n" +
-									"1 - Get a ticket\n" +
-									"2 - Employee actions\n" +
-									"3 - Manager actions\n" +
-									"4 - Exit ( JUST FOR DEMO )");
-							input = scanner.nextInt(); //Get input from console
-							//Menu handler
-							switch (input){
-								case 1:
-									Ticket t = service.getTicket();
-									System.out.println(t);
-									break;
-								case 2:
-									System.out.println("1 - Call next customer\n");
-									System.out.println("2 - Return to Main Menu\n");
-									input = scanner.nextInt();
-									switch (input){
-										case 1:
-											System.out.println("What's your counter number?");
-											service.callNextCustomer(scanner.nextInt());
-											break;
-										default:
-											break;
-									}
-									break;
-
-								case 3:
-									//Manager actions
-									System.out.println("Type query option followed by time filter (d,w,m)\n" +
-											"Example: 1 d\n" +
-											"1 - How many customers have been served for each request type\n" +
-											"2 - The number of customers each counter has served divided by request type");
-									//Getting input
-									input = scanner.nextInt();
-									char filter = scanner.next().charAt(0);
-									if (input == 1) service.printAllStats(filter);
-									else if(input == 2) service.printAllStatsByCounter(filter);
-									else System.err.println("Error on input");
-									break;
-								case 4:
-									running = false;
-									running_date = false;
-									System.out.println("Shutting down ...");
-									break;
-							}
-						}
+						Ticket t = service.getTicket();
+						System.out.println(t);
 						break;
 					case 2:
-						running_date = false;
+						System.out.println("1 - Call next customer\n");
+						System.out.println("2 - Return to Main Menu\n");
+						input = scanner.nextInt();
+						switch (input){
+							case 1:
+								System.out.println("What's your counter number?");
+								service.callNextCustomer(scanner.nextInt());
+								break;
+
+							default:
+								break;
+						}
+						break;
+
+					case 3:
+						//Manager actions
+						System.out.println("Type query option followed by time filter (d,w,m)\n" +
+								"Example: 1 d\n" +
+								"1 - How many customers have been served for each request type\n" +
+								"2 - The number of customers each counter has served divided by request type");
+						//Getting input
+						input = scanner.nextInt();
+						char filter = scanner.next().charAt(0);
+						if (input == 1) service.printAllStats(filter);
+						else if(input == 2) service.printAllStatsByCounter(filter);
+						else System.err.println("Error on input");
+						break;
+					case 4:
+						running = false;
 						System.out.println("Shutting down ...");
 						break;
+					default:
+						System.out.println("Error code inserted. Try again\n");
+						break;
 				}
-
 			}
 
 
